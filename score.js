b9b8ed6847223e767ea05e3750b17d0af3facaba
thomassth <thomassth@users.noreply.github.com>
2015-09-29 17:10:01 +0800

    Pics, info

--- score.js
+++ score.js
@@ -89,3 +89,6 @@ function medal(){
         break;
     }
 }
+function whyVideo(){
+    	document.getElementsByClassName("ytp-thumbnail-overlay ytp-cued-thumbnail-overlay").setAttribute="style",'background-image: url("https://i.ytimg.com/vi/sOLgRQ74vHw/hqdefault.jpg");';
+}
