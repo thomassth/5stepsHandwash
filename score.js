4d8832ca9584822b56af90f5adf379e91986ec64
thomassth <thomassth@users.noreply.github.com>
2015-10-02 14:40:39 +0800

    svg to png

--- score.js
+++ score.js
@@ -83,22 +83,22 @@ function medal(){
     var x=Number(getCookie("score"));
     switch(true){
         case (x>=4500&&x<9000):
-        document.getElementById("medal").setAttribute("src","img/medals/bronze.svg");
+        document.getElementById("medal").setAttribute("src","img/medals/bronze.png");
         document.getElementById("medalTag").innerHTML="銅獎";
         setCookie("medal", 1, 365);
         break;
         case (x>=9000&&x<13500):
-        document.getElementById("medal").setAttribute("src","img/medals/silver.svg");
+        document.getElementById("medal").setAttribute("src","img/medals/silver.png");
         document.getElementById("medalTag").innerHTML="銀獎";
         setCookie("medal", 2, 365);
         break;
         case (x>=13500&&x<18000):
-        document.getElementById("medal").setAttribute("src","img/medals/gold.svg");
+        document.getElementById("medal").setAttribute("src","img/medals/gold.png");
         document.getElementById("medalTag").innerHTML="金獎";
         setCookie("medal", 3, 365);
         break;
         case (x>=18000):
-        document.getElementById("medal").setAttribute("src","img/medals/platinum.svg");
+        document.getElementById("medal").setAttribute("src","img/medals/platinum.png");
         document.getElementById("medalTag").innerHTML="白金獎";
         setCookie("medal", 4, 365);
         break;
